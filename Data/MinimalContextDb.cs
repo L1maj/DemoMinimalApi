c170d936f66a0062c1714bbead47fcf0198b8c07
jaelson lima <jaelsonlima_93@hotmail.com>
2023-10-03 17:04:28 -0300

    iicial projeto minimalApi

---
+++ Data/MinimalContextDb.cs
@@ -0,0 +1,32 @@
+﻿using DemoMinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMinimalApi.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .HasKey(e => e.Id);
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("Fornecedores");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
